72bffcc65e27d3a8d989f10c1f9fd89ac383b37f
Vitalij <vitalij.cernysiov@gmail.com>
2023-01-29 20:00:45 +0200

    update

--- src/Components/Line.jsx
+++ src/Components/Line.jsx
@@ -13,26 +13,50 @@ function Line  ({ consum, endDate }) {
     for(let i = 1 ; i <= (consum.duration)+1; i++) 
     lineNumber.push(i) ;
 
+   
 
-    let date = new Date();
+
+    let date = new Date(consum.startDate);
     let dt = date.getDate();
     date.setDate(23);
-    if (dt <= 23) {
-      date.setMonth(date.getMonth() + 1);
+    if (dt < 23) {
+      date.setMonth(date.getMonth()-1);
     }
     date.setHours(23, 59, 59, 0);
     let start = moment(date).format('L')
     let starts = new Date(start);
     let startDates = new Date(consum.startDate);
     let difference = Math.abs(starts - startDates);
-    const diffDays = Math.ceil(difference/ (1000 * 60 * 60 * 24))
-    
+    const diffDays = Math.ceil(difference/ (1000 * 60 * 60 * 24));
+    let endDays = new Date(endDate);
+
     let rewarddate = [];
     let rewarddates = [];
-    for (let i = 1; i <= (consum.duration); i++)
+    let days = [];
+    let dif = [];
+    let startis = [];
+    let endArr = [];
+    let daysdifferent = [];
+    for (let i = 1; i <= (consum.duration)+1; i++){
+    rewarddates[i] = moment(starts.setMonth(starts.getMonth())).format('L');
+    rewarddates[1] = (consum.startDate);
+    // rewarddates[consum.duration + 1] = endDate;
     rewarddate[i] = moment(starts.setMonth(starts.getMonth()+1)).format('L');
-    rewarddates = rewarddate.push((endDate));
-    
+    rewarddate[consum.duration+1] = endDate;
+    days[i] = moment(starts.setMonth(starts.getMonth())).format('L');
+    days[consum.duration+1] = endDays;
+    startis[i] = new Date(days[i]);
+    endArr[i] = new Date(rewarddates[i])
+    dif[i] = Math.abs(startis[i] - endArr[i]);
+    daysdifferent[i] = Math.ceil(dif[i]/ (1000 * 60 * 60 * 24));
+    }
+
+    // let dates = moment(date).format('L');
+    // let date2 = moment(date).format('L');
+    // let daydif = [];
+    // for(let i = 1 ; i <= (consum.duration)+1; i++) {
+    // dates = new Date(rewarddates[i]);
+    // date2 = new Date(rewarddates[i+1])
 
     let rewardrate = [];
     for (let i = 1; i <= (consum.duration)+1; i++){
@@ -40,27 +64,49 @@ function Line  ({ consum, endDate }) {
     else {rewardrate[i] = (10).toFixed(2) + '%'} }
     
 
-    let additional = Number((consum.amount)/100*(consum.rewardRate)/365*30).toFixed(6);
+    // let additional = Number((consum.amount)/100*(consum.rewardRate)/365*daysdifferent[i]).toFixed(6);
     let amountToDate = [];
     let todate = [];
+    let pavr = [];
+    let zero = 0;
     for (let i = 1; i <= (consum.duration)+1; i++) {
-    amountToDate[i] =((additional)*i).toFixed(6);
-    todate[i] =((additional)*i)-1;
+    amountToDate[i] =((consum.amount)/100*(consum.rewardRate)/365*daysdifferent[i]).toFixed(6);
+    todate[i] =((consum.amount)/100*(consum.rewardRate)/365*daysdifferent[i]);
+    todate[0] = zero;
+    // pavr =  todate.reduce(previousValue, currentValue) => (previousValue + currentValue)
     }
     
     let investmentamount = [];
     for(let i = 1 ; i <= (consum.duration)+1; i++){
-     if (consum.reinvest === 1) {investmentamount[i] = Number((consum.amount) + todate[i]+1).toFixed(6)}
+     if (consum.reinvest === 1) {investmentamount[i] = Number((consum.amount) + todate[i]).toFixed(6)}
      else {investmentamount[i] = Number(consum.amount).toFixed(6)};
     }
 
     let totalrewardamount = [];
     for (let i = 1; i <= (consum.duration)+1; i++){ 
-    totalrewardamount[i] = (++todate[i]).toFixed(6);
+    totalrewardamount[i] += todate[i]++;
     }
+
+    // [0, 1, 2, 3, 4].reduce(function(previousValue, currentValue, currentIndex, array) {
+    //   console.log(previousValue + currentValue);
+    // });
+
+    // let sum = [];
+    // for (let i = 0; i < 
+    //   todate.length; i++) {
+    //   // take every item in the array and add it to sum variable
+    //   sum[i] += todate[i]
+      // initial: sum = 0 
+      // iteration 1: 0 + 1 => sum = 1
+      // iteration 2: 1 + 4 => sum = 5
+      // iteration 3: 5 + 0 => sum = 5
+      // iteration 4: 5 + 9 => sum = 14
+      // iteration 5: 14 + -3 => sum = 11
+    
+      // }
     let heading = ['Line #', 'Reward Date', 'Investment amount', 'Reward amount', 'Total Reward Amount to Date', 'Stacking Reward Rate' ];
     
-    console.log(rewarddate, rewarddates)
+    console.log(todate, totalrewardamount);
 
 
     return (
